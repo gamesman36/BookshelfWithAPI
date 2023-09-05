b34ff62c9ccacaf9c19527031c35de818e12bcb3
Daniel Andreas Wang <gamesman36@outlook.com>
2023-09-05 13:28:16 +0200

    JSON and styling

--- BookshelfWithAPI/Program.cs
+++ BookshelfWithAPI/Program.cs
@@ -1,14 +1,25 @@
+using System.Text.Json;
 using BookshelfWithAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/bookshelf", () =>
+var filename = "books.json";
+if (!File.Exists(filename))
 {
-    return new Book[]
+    var books = new Book[]
     {
         new Book("1", "Javaprogrammering kort og godt", "Knut Hegna, Arne Maus", "Computing", "2017", "Universitetsforlaget", "05/09/2023")
     };
+    
+    var json = JsonSerializer.Serialize(books);
+    File.WriteAllText(filename, json);
+}
+
+app.MapGet("/bookshelf", () =>
+{
+    var json = File.ReadAllText(filename);
+    return JsonSerializer.Deserialize<Book[]>(json);
 });
 
 app.UseStaticFiles();
