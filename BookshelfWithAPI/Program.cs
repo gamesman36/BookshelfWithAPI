6105b608600466a0f0119353263557a1785a7242
Daniel Andreas Wang <gamesman36@outlook.com>
2023-09-05 14:45:10 +0200

    Saving work

--- BookshelfWithAPI/Program.cs
+++ BookshelfWithAPI/Program.cs
@@ -6,11 +6,25 @@ var app = builder.Build();
 
 var filename = "books.json";
 
-app.MapGet("/bookshelf", () =>
+app.MapGet("/bookshelf", GetAllBooks);
+app.MapPost("/bookshelf", (Book book) =>
 {
-    var json = File.ReadAllText(filename);
-    return JsonSerializer.Deserialize<Book[]>(json);
+    var books = GetAllBooks().ToList();
+    books.Add(book);
+    SaveToFile(books);
 });
 
 app.UseStaticFiles();
 app.Run();
+
+Book[] GetAllBooks()
+{
+    var json = File.ReadAllText(filename);
+    return JsonSerializer.Deserialize<Book[]>(json);
+}
+
+void SaveToFile(IEnumerable<Book> books)
+{
+    var json = JsonSerializer.Serialize(books);
+    File.WriteAllText(filename, json);
+}
