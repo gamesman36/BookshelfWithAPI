eb72da11ec3faedfe375c1fb2ba23d6427eec97d
Daniel Andreas Wang <gamesman36@outlook.com>
2023-09-05 12:18:26 +0200

    Minor entry correction

--- BookshelfWithAPI/Program.cs
+++ BookshelfWithAPI/Program.cs
@@ -7,7 +7,7 @@ app.MapGet("/bookshelf", () =>
 {
     return new Book[]
     {
-        new Book("1", "Javaprogrammering kort og godt", "Knut Hegna, Arne Maus", "Cpmputing", "2017", "Universitetsforlaget", "05/09/2023")
+        new Book("1", "Javaprogrammering kort og godt", "Knut Hegna, Arne Maus", "Computing", "2017", "Universitetsforlaget", "05/09/2023")
     };
 });
 
