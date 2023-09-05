d57bd365afdf62b098c97c3e677535796f50cc34
Daniel Andreas Wang <gamesman36@outlook.com>
2023-09-05 13:45:50 +0200

    Removing unnecessary code

--- BookshelfWithAPI/Program.cs
+++ BookshelfWithAPI/Program.cs
@@ -5,16 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var filename = "books.json";
-if (!File.Exists(filename))
-{
-    var books = new Book[]
-    {
-        new Book("1", "Javaprogrammering kort og godt", "Knut Hegna, Arne Maus", "Computing", "2017", "Universitetsforlaget", "05/09/2023")
-    };
-    
-    var json = JsonSerializer.Serialize(books);
-    File.WriteAllText(filename, json);
-}
 
 app.MapGet("/bookshelf", () =>
 {
